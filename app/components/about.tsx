a56f5bc54467a20cfdf0fe3693af3e7e77fc2c96
syedubaid <syedabuubaid@gmail.com>
2025-05-15 12:27:55 +0530

    commit

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -3,7 +3,8 @@ export const About=()=>{
         <div className="h-[400px] w-max-screen p-[60px] px-[120px] mt-[50px]">
             <div className="font-bold text-2xl tracking-tight">what is runix?</div>
             <div className="tracking-tighter text-xl">runix allows users to deploy their code seamlessly with minimal time , with efficient workflow and scalable architecture our design handles everything.
-                <br/>runix currently supports only Javascript and its related frameworks for deploying.
+                runix currently supports only Javascript and its <br/>related frameworks for deploying.
+                <br/>Uses redis and ecs for scalable and efficient approach and maintains a persistent websocket connection to serve logs to the user for debugging and error management.
             </div>
         </div>
     )
